09fba56601f2d44ec75622f56b40398c2f6380b0
yabetsalelign <149236920+yabetsalelign@users.noreply.github.com>
2025-02-14 02:31:49 -0800

    Update sum_finder.py

--- Week2/sum_finder.py
+++ Week2/sum_finder.py
@@ -1,1 +1,9 @@
+def sum_finder(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    return total
+
+numbers = [1, 2, 3, 4, 5]
+print("The sum of the given list of numbers is:", sum_finder(numbers))
 
