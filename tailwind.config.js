9add203443e2f72c99ca6483b927a818d0c88425
Yabets Alelign <yabetsalelign@gmail.com>
2025-04-06 10:58:42 -0700

    Added different components

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',  // Ensure all JS, JSX, TS, and TSX files in src are included
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
